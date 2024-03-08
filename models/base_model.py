631a25c7672b4c8592502cb7ece1128e2d31459f
Lamba237 <simontanyi2004@gmail.com>
2024-03-08 07:28:31 -0800

    updates

--- models/base_model.py
+++ models/base_model.py
@@ -5,14 +5,17 @@
 import uuid
 from datetime import datetime
 
-import models
-
 
 class BaseModel:
-    created_at = datetime.now()
-    updated_at = datetime.now()
-
+    """
+    a class BaseModel that defines all
+    common attributes/methods for other classes
+    """
     def __init__(self, *args, **kwargs):
+        """
+        :param kwargs: holds all pass argument strings.
+        """
+
         if kwargs:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
@@ -21,26 +24,33 @@ class BaseModel:
                     setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
-            self.created_at = BaseModel.created_at
-            self.updated_at = BaseModel.updated_at
-            models.storage.new(self)
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        """
+        :return: prints out a class name,
+        followed by the id and a dictionary
+        """
+        holder = "[{}] ({}) {}"
+        return holder.format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        """Method to update the public instance attribute "updated_at"\
-            With the current datetime"""
+        """
+        Method to update the public instance attribute
+        "updated_at"\
+        With the current datetime
+        """
         self.updated_at = datetime.now()
-        models.storage.save()
 
     def to_dict(self):
-        """Method to generate a dictionary representation\
-            of the instance BaseModel"""
+        """
+        :return: a dictionary containing all
+        the key value pairs
+        """
         class_name = self.__class__.__name__
         instance_dict = {key: value for key, value in vars(self).items()}
         instance_dict["__class__"] = class_name
         instance_dict["created_at"] = datetime.isoformat(self.created_at)
         instance_dict["updated_at"] = datetime.isoformat(self.updated_at)
         return instance_dict
-
